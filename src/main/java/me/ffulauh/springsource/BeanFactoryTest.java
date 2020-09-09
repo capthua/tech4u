a8a8a57ea4f4e4312c959315a6794998086644e7
hansh <hansh@ilinecn.com>
2020-09-09 18:58:28 +0800

    886

--- src/main/java/me/ffulauh/springsource/BeanFactoryTest.java
+++ src/main/java/me/ffulauh/springsource/BeanFactoryTest.java
@@ -1,6 +1,8 @@
 package me.ffulauh.springsource;
 
 
+import me.ffulauh.domain.A;
+import me.ffulauh.domain.B;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
@@ -8,11 +10,16 @@ import org.springframework.core.io.ClassPathResource;
 public class BeanFactoryTest {
 
     public static void main(String[] args) {
-        BeanFactory bf=new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+        BeanFactory bf=new XmlBeanFactory(new ClassPathResource("circularRefTest.xml"));
+//        BeanFactory bf=new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
 //        ApplicationContext bf=new ClassPathXmlApplicationContext("beanFactoryTest.xml");
-        MyTestBean bean=(MyTestBean) bf.getBean("myTestBean");
-        String hehe=bean.getTestStr();
-        System.out.println(hehe);
+//        MyTestBean bean=(MyTestBean) bf.getBean("a");
+//        String hehe=bean.getTestStr();
+
+        A a=(A) bf.getBean("a");
+        B b=(B) bf.getBean("b");
+
+        System.out.println("hehe");
     }
 
 }
