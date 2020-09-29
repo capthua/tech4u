33cce69344ec3f84704cdd7462d6816fd0270618
capthua <capthua24@gmail.com>
2020-09-29 22:08:52 +0800

    将springframework版本改为3.1

--- src/main/java/me/ffulauh/springsource/postprocessor/PropertyConfigurerDemo.java
+++ src/main/java/me/ffulauh/springsource/postprocessor/PropertyConfigurerDemo.java
@@ -3,14 +3,18 @@ package me.ffulauh.springsource.postprocessor;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class PropertyConfigurerDemo {
 
     public static void main(String[] args) {
-        ConfigurableListableBeanFactory bf=new XmlBeanFactory(new ClassPathResource("beanFactoryPostProcessorTest.xml"));
-        BeanFactoryPostProcessor bfpp=(BeanFactoryPostProcessor)bf.getBean("bfpp");
-        bfpp.postProcessBeanFactory(bf);
-        System.out.println(bf.getBean("simpleBean"));
+//        ConfigurableListableBeanFactory bf=new XmlBeanFactory(new ClassPathResource("beanFactoryPostProcessorTest.xml"));
+        ApplicationContext context=new ClassPathXmlApplicationContext("beanFactoryPostProcessorTest.xml");
+
+        BeanFactoryPostProcessor bfpp=(BeanFactoryPostProcessor)context.getBean("bfpp");
+//        bfpp.postProcessBeanFactory(bf);
+        System.out.println(context.getBean("simpleBean"));
     }
 }
