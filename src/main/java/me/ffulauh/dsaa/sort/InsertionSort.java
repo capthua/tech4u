732eb3a9c96f885d049dcddcb7196439aeb1e4c2
hansh <hansh@ilinecn.com>
2021-02-04 16:45:06 +0800

    添加插入排序

--- src/main/java/me/ffulauh/dsaa/sort/InsertionSort.java
+++ src/main/java/me/ffulauh/dsaa/sort/InsertionSort.java
@@ -1,24 +1,25 @@
 package me.ffulauh.dsaa.sort;
 
-public class InsertionSort extends BaseSort{
-
-    public static int[] insertionSort(int[] nums){
-        int l=nums.length;
-        for(int p=1;p<l-1;p++){
-            for(int j=p;j>0;j--){
-                if(nums[j]<nums[j-1]){
-                    int t=nums[j];
-                    nums[j]=nums[j-1];
-                    nums[j-1]=t;
-                }
-            }
-        }
-        return nums;
-    }
+public class InsertionSort extends BaseSort {
 
     public static void main(String[] args) {
-        int[] nums=new int[]{9,3,1,5,7,6};
-        nums=insertionSort(nums);
+        int[] nums = new int[]{51, 2, 7, 14, 8, 9};
+        insertionSort(nums);
         printNums(nums);
+    }
+
+    /**
+     * 位置p上的元素存储于tmp, 而(在位置p之前)所有更大的元素都向右移动一个位置. 然后tmp被置于正确的位置上.
+     * @param nums
+     */
+    public static void insertionSort(int[] nums) {
+        int j;
+        for (int p = 1; p < nums.length; p++) {
+            int tmp = nums[p];
+            for (j = p; j > 0 && tmp < nums[j - 1]; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[j] = tmp;
+        }
     }
 }
