fd0e501353bb9b1fe19a22d1cf34826eab31f37b
hansh <hansh@ilinecn.com>
2021-04-01 15:59:12 +0800

    wx

--- src/main/java/me/ffulauh/javalang/collectiondemo/map/HashMapDemo.java
+++ src/main/java/me/ffulauh/javalang/collectiondemo/map/HashMapDemo.java
@@ -1,12 +1,24 @@
 package me.ffulauh.javalang.collectiondemo.map;
 
+
 import java.util.HashMap;
-import java.util.Map;
 
 public class HashMapDemo {
     public static void main(String[] args) {
-        Map<String,String> map=new HashMap<>();
-        map.put("han","shaohua");
-        map.containsKey("han");
+        People p1=new People();
+        p1.setName("han");
+        p1.setAge(21);
+        People p2=new People();
+        p2.setName("han");
+        p2.setAge(21);
+        HashMap<People,String> map=new HashMap<>();
+        map.put(p1,"han");
+        map.put(p2,"ht");
+        map.get(p2);
+        People p;
+        if((p=p1)!=null){
+
+        }
+        System.out.println("hehe");
     }
 }
---
+++ src/main/java/me/ffulauh/javalang/collectiondemo/map/People.java
@@ -0,0 +1,43 @@
+package me.ffulauh.javalang.collectiondemo.map;
+
+import java.util.Objects;
+
+public class People {
+
+    private Integer age;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        People people = (People) o;
+
+        if (!Objects.equals(age, people.age)) return false;
+        return Objects.equals(name, people.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = age != null ? age.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
